6680cd59fa27c1c78cb24c775383cc6d45512cc0
joaopedro384 <joao.lsantos@bandtec.com.br>
2021-03-05 15:18:35 -0300

    BD atualizado

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -24,5 +24,9 @@ SELECT * FROM Cadastro;
 
 -- Inserir dados na tabela.
 INSERT INTO Cadastro VALUES 
-(null,'João Carlos','84511043043','1972-07-15','JoaoCarlos06@outlook.com','11912345678','88816210','Rua Antônio de Morais','24','São Paulo','Sorocaba'),
-(null,'Wellison Costa','79539874068','1965-03-25','Wellison.Costa@outlook.com','321234569','71593675','Rua Bem-te-vi','354','Minas Gerais','Montes Claros');
+(null,'João Carlos','84511043043','1972-07-15','JoaoCarlos06@outlook.com','11912345678','88816210','Rua Antônio de Morais',24,'São Paulo','Sorocaba'),
+(null,'Wellison Costa','79539874068','1965-03-25','Wellison.Costa@outlook.com','321234569','71593675','Rua Bem-te-vi',354,'Minas Gerais','Montes Claros'),
+(null, 'José Felipe', '31938319052', '1985-06-14', 'josefelipe@gmail.com', '21931215321', '27555970', 'Avenida Boa Vista', 23, 'Rio de Janeiro', 'Engenheiro Passos'),
+(null, 'Nicolas Silva' , '37010350086', '1990-04-07', 'nicolassilva90@uol.com.br', '51987452125', '47813108', 'Rua Antônio Lúcio Peixoto', 1425, 'Bahia', 'Barreiras');
+
+
